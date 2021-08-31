9affb53ab3027aa21a6d67dbd7edbbfa4f45dc5d
sergey32167 <serg-li-@mail.ru>
2021-08-31 22:21:42 +0300

    add program file

---
+++ src/test/java/MainPageTest.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class MainPageTest {
+
+    @Test
+    public void openChromePage() {
+        System.out.println("Selenium test has been started!");
+        WebDriver driver = new BrowserService("chrome").getDriver();
+        driver.get("http://google.com");
+
+        System.out.println(driver.getTitle());
+        Assert.assertEquals(driver.getTitle(), "Google");
+        driver.close();
+    }
+
+}
